2102b0b348eacd09431e30a4c67e22d390d03d22
JanJared58 <janjaredrmz32@gmail.com>
2025-03-09 20:16:00 -0600

    ej3

---
+++ Ejercicio 3.py
@@ -0,0 +1,20 @@
+def contar_vocales_consonantes(palabra):
+
+    vocales = "aeiouAEIOU"
+    
+    num_vocales = 0
+    num_consonantes = 0
+    
+    for letra in palabra:
+        if letra.isalpha(): 
+            if letra in vocales:
+                num_vocales += 1
+            else:
+                num_consonantes += 1
+
+    return num_vocales, num_consonantes
+
+
+palabra = input("Por favor, ingresa una palabra: ")
+vocales, consonantes = contar_vocales_consonantes(palabra)
+print(f"La palabra '{palabra}' contiene {vocales} vocales y {consonantes} consonantes.")
